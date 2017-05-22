f64f347c1e1dcbf04f0036777b657523e7fe6ee6
Chaitanya <mnsvkchaitanya@gmail.com>
2017-05-22 20:28:13 +0530

    Displaying images.

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,3 @@
-package excelReader.excelReader;
-
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Frame;
@@ -30,8 +28,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.swing.JLabel;
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
+import javax.swing.JOptionPane;
 import javax.swing.ImageIcon;
 
 public class Application extends Frame {
@@ -68,37 +65,38 @@ public class Application extends Frame {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frame.setLayout(null);
 		addIcrisatLogo();
 
 		JLabel lblFile = new JLabel("File Name");
 		lblFile.setFont(new Font("Arial", Font.BOLD | Font.ITALIC, 20));
 		lblFile.setBounds(10, 141, 121, 20);
-		frame.getContentPane().add(lblFile);
+		frame.add(lblFile);
 
 		final JTextField textField = new JTextField();
 		textField.setBounds(141, 144, 334, 20);
-		frame.getContentPane().add(textField);
+		frame.add(textField);
 		textField.setColumns(10);
 
 		JButton btnSubmit = new JButton("Submit");
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				convert(textField.getText());
+				String status = convert(textField.getText());
+				JOptionPane.showMessageDialog(frame, status);
 			}
 		});
 		btnSubmit.setBounds(256, 191, 89, 23);
-		frame.getContentPane().add(btnSubmit);
+		frame.add(btnSubmit);
 
 		JLabel lblFileConverter = new JLabel("File Converter");
 		lblFileConverter.setFont(new Font("Tahoma", Font.BOLD, 25));
 		lblFileConverter.setForeground(Color.BLUE);
 		lblFileConverter.setBounds(195, 32, 210, 44);
-		frame.getContentPane().add(lblFileConverter);
+		frame.add(lblFileConverter);
 		
 		JLabel lblhornysaze = new JLabel("@hornySaze");
 		lblhornysaze.setBounds(10, 336, 101, 14);
-		frame.getContentPane().add(lblhornysaze);
+		frame.add(lblhornysaze);
 		
 		JButton btnBrowse = new JButton("Browse");
 		btnBrowse.addActionListener(new ActionListener() {
@@ -108,7 +106,7 @@ public class Application extends Frame {
 			}
 		});
 		btnBrowse.setBounds(485, 143, 89, 23);
-		frame.getContentPane().add(btnBrowse);
+		frame.add(btnBrowse);
 
 	}
 
@@ -119,7 +117,8 @@ public class Application extends Frame {
 }
 
 	
-	public static void convert(String fileName) {
+	public static String convert(String fileName) {
+		String status = "Success";
 		String outputFile = fileName.substring(0, fileName.lastIndexOf(".")) + "_processed"
 				+ fileName.substring(fileName.lastIndexOf("."));
 		XSSFWorkbook outputWorkbook = new XSSFWorkbook();
@@ -147,15 +146,17 @@ public class Application extends Frame {
 					colNum = 0;
 				}
 			}
-
 			FileOutputStream outputStream = new FileOutputStream(outputFile);
 			outputWorkbook.write(outputStream);
 			outputWorkbook.close();
 			workbook.close();
+			return status;
 		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+			status = "Cant find the file at location"+fileName;
+			return status;
+		} catch (Exception e) {
+			status = "Exception reading file.\n";
+			return status;
 		}
 	}
 
@@ -172,12 +173,12 @@ public class Application extends Frame {
 	public void addIcrisatLogo() {
 		JLabel icrisatLogoLabel = new JLabel("");
 		
-		URL url = Application.class.getResource("icrisat.png");
+		URL url = Application.class.getResource("/icrisat.png");
 		if(url != null){
 			ImageIcon icon = new ImageIcon(new ImageIcon(url).getImage().getScaledInstance(150, 50, Image.SCALE_DEFAULT));
 			icrisatLogoLabel.setIcon(icon);
 			icrisatLogoLabel.setBounds(407, 286, 167, 50);
-			frame.getContentPane().add(icrisatLogoLabel);
+			frame.add(icrisatLogoLabel);
 		}
 	}
 }
